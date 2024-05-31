bf1eb14d6cccba4834f2e1cb4377349254a7909f
Tomás Moiano Pereira <90468341+TomasMoiano@users.noreply.github.com>
2024-05-31 13:59:14 -0300

    sdc

--- pagina-sobre/script_popup_cookies.js
+++ pagina-sobre/script_popup_cookies.js
@@ -3,7 +3,7 @@ function aceitarCookies() {
     // Aqui você pode adicionar ações adicionais após o usuário aceitar cookies
 }
 
-function rejeitarCookies() {
+function privacidade() {
     document.querySelector('.cookie-popup').style.display = 'none';
     // Aqui você pode adicionar ações adicionais após o usuário rejeitar cookies
 }
