50374257ab8b9e95d2662eebecceb0de07609a02
Marcos Vinicius Hernandes Da Silva Carreiro <35454194+04vini@users.noreply.github.com>
2024-06-07 21:32:03 -0300

    footer.php

---
+++ tela-de-login-blog/footer.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php include_once "../template/head.php" ?>
+	<style>
+	</style>
+	<!-- Bootstrap CSS -->
+	<link href="../assets/css/main.min.css?t=1712110939880" rel="stylesheet" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+	<title>footer</title>
+</head>
+<body>
+
+
+<div class="container">
+  <!-- Footer -->
+  <footer class="text-center text-white" style="background-color: #3f51b5">
+    <!-- Grid container -->
+    <div class="container">
+      <!-- Section: Links -->
+      <section class="mt-5">
+        <!-- Grid row-->
+        <div class="row text-center d-flex justify-content-center pt-5">
+          <!-- Grid column -->
+          <div class="col-md-2">
+            <h6 class="text-uppercase font-weight-bold">
+              <a href="#!" class="text-white">Sobre</a>
+            </h6>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2">
+            <h6 class="text-uppercase font-weight-bold">
+              <a href="#!" class="text-white">Cursos</a>
+            </h6>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2">
+            <h6 class="text-uppercase font-weight-bold">
+              <a href="#!" class="text-white">Banco de Vagas</a>
+            </h6>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2">
+            <h6 class="text-uppercase font-weight-bold">
+              <a href="#!" class="text-white">Blog</a>
+            </h6>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2">
+            <h6 class="text-uppercase font-weight-bold">
+              <a href="#!" class="text-white">Contato</a>
+            </h6>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row-->
+      </section>
+      <!-- Section: Links -->
+
+      <hr/>
+
+      <!-- Section: Text -->
+      <section>
+        <div class="row d-flex justify-content-center">
+          <div class="col-lg-8">
+            <p>
+              ACEDA - Associação Comercial e Empresarial do Distrito Anhanguera.
+              <br>
+              Endereço: Sol Nascente - Cursos e Treinamento - Av. Euclides da Cunha, 705 - Res. Sol Nascente, São Paulo - SP, 05281-070
+              <br>
+              Telefone: (11) 95877-1996
+              <br>
+              <br>
+              <div style="width: 100%"><iframe width="100%" height="250" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=250&amp;hl=en&amp;q=Sol%20Nascente%20-%20Cursos%20e%20Treinamento%20-%20Av.%20Euclides%20da%20Cunha,%20705%20-%20Res.%20Sol%20Nascente,%20S%C3%A3o%20Paulo%20-%20SP,%2005281-070+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps vehicle tracker</a></iframe></div>
+            </p>
+          </div>
+        </div>
+      </section>
+      <!-- Section: Text -->
+
+      <!-- Section: Social -->
+      <section class="text-center"> 
+        <i class="bi bi-facebook me-5" style ="font-size: 2rem;"></i>
+        <i class="bi bi-twitter-x me-5" style ="font-size: 2rem;"></i>
+        <i class="bi bi-instagram me-5" style ="font-size: 2rem;"></i>
+        <i class="bi bi-linkedin me-5" style ="font-size: 2rem;"></i>
+      </section>
+      <!-- Section: Social -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div
+         class="text-center p-3"
+         style="background-color: rgba(0, 0, 0, 0.2)"
+         >
+      ©2024 Copyright:
+      <a class="text-white" href="#"
+         >ACEDA</a
+        >
+    </div>
+    <!-- Copyright -->
+  </footer>
+</body>
+</html>
