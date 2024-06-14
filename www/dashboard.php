b9c62d14c579bfb6172e1a1ff80e02939a4ddb6e
Ronaldo Lacerda <ronaldolacerdadasilva@gmail.com>
2024-06-13 23:45:07 -0300

    Update home - adm

--- www/dashboard.php
+++ www/dashboard.php
@@ -18,6 +18,7 @@ if((!isset($_SESSION['id'])) AND (!isset($_SESSION['nome']))){
     <title>Home - Administrador</title>
     <link href="./assets/css/main.min.css?t=1712110939880" rel="stylesheet" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
@@ -25,11 +26,27 @@ if((!isset($_SESSION['id'])) AND (!isset($_SESSION['nome']))){
     <!-- NavBar -->
 	<?php include_once "./template/navbar-adm.php" ?>
 	<!-- NavBar -->
-     <div class="container m-5">
-        <h3>Bem vindo <?php echo $_SESSION['nome']; ?>!</h1>
 
-        <a class="btn btn-warning rounded-pill" href="sair.php">Sair</a>
-    </div>
+    <div class="container">
+        <div class="alert alert-success mt-5 row justify-content-md-center">
+            <div class="col-md-auto">
+                <h4>Bem vindo <?php echo $_SESSION['nome']; ?>!</h1>
+            </div>
+        </div>  
+        
+        <div class="d-flex justify-content-end dropdown">
+            <button class="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Relatórios
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Formulário EmpreendAceda</a></li>
+                <li><a class="dropdown-item" href="#">Formulário Solicitações</a></li>
+            </ul>
+        </div>
+            
+        
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
-</html>
+</html> 
