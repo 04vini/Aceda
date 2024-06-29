4cd28837944a814b60e761e76a818a26207353b7
Tomás Moiano Pereira <90468341+TomasMoiano@users.noreply.github.com>
2024-06-29 12:37:46 -0300

    Atualização página HOME-carrosel e serviços aceda título

--- www/home.php
+++ www/home.php
@@ -72,7 +72,6 @@ $dadosblog = mysqli_query($conn, $queryblog);
 						}
 					}
 					?>
-<<<<<<< HEAD
 
 					<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
 						data-bs-slide="prev">
@@ -84,8 +83,6 @@ $dadosblog = mysqli_query($conn, $queryblog);
 						<span class="carousel-control-next-icon" aria-hidden="true"></span>
 						<span class="visually-hidden">Next</span>
 					</button>
-=======
->>>>>>> 67f9b965632d36f189247e6451646888319f0e6b
 				</div>
 				<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
 					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
