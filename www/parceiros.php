1fcf763f5ea862a3291f2a62f5621d6c44c79534
Tomás Moiano Pereira <90468341+TomasMoiano@users.noreply.github.com>
2024-07-08 16:49:17 -0300

    Update parceiros.php

--- www/parceiros.php
+++ www/parceiros.php
@@ -1,102 +1,105 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap CSS -->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Bootstrap CSS -->
 	<link href="./assets/css/main.min.css?t=1712110939880" rel="stylesheet" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.positus.global/production/resources/robbu/whatsapp-button/whatsapp-button.css">
 	<link rel="icon" type="image/x-icon" href="./assets/img/favicons/android-icon-48x48.png">
 	<title>Parceiros | ACEDA</title>
 	<style>
-        body {
+		body {
 			overflow-x: hidden;
 		}
-    </style>
+	</style>
 </head>
-<body class= "bg-primary bg-opacity-50" style="font-family: Outfit;">
+
+<body class="bg-primary bg-opacity-50" style="font-family: Outfit;">
 	<!-- NavBar -->
 	<?php include_once "./template/navbar.php" ?>
 	<!-- NavBar -->
-    <!-- Principais parceiros -->
-	
-		<div class="bg-primary p-5 mt-3">
-		    <h2 class="text-white">Nossos Parceiros</h2>
-	    </div>
-	    <section class="mt-0">
-	        <div class="container">
-    			<div class="row">
-    				<div class="col text-center border border-gray border-1 rounded-4 m-1 bg-white p-1">
-    					<img src="./assets/img/cinternet.png" class="img-fluid rounded-pill" alt="">
-    					<h3>Creative Internet</h3>
-    					<p class="pt-5" >A ACEDA através de sua parceira com a Creative Internet concede a seus associados o benefício de Upload = Download (Mesma velocidade para baixar e subir seus arquivos, fotos,
+	<!-- Principais parceiros -->
+
+	<div class="bg-primary p-5">
+		<h2 class="text-white fw-semibold">Nossos Parceiros</h2>
+	</div>
+	<section class="mt-0">
+		<div class="container">
+			<div class="row">
+				<div class="col text-center border border-gray border-1 rounded-4 m-1 bg-white p-1">
+					<img src="./assets/img/cinternet.png" class="img-fluid rounded-pill" alt="">
+					<h3>Creative Internet</h3>
+					<p class="pt-5">A ACEDA através de sua parceira com a Creative Internet concede a seus associados o benefício de Upload = Download (Mesma velocidade para baixar e subir seus arquivos, fotos,
 						vídeos etc.).
-						</p>
-    				</div>
-    				<div class="col text-center border border-gray border-1 rounded-4 m-1 bg-white pt-5">
-    					<img src="./assets/img/3tellogo.png" class="img-fluid rounded-pill" alt="">
-    					<h3>3 Tel</h3>
-    					<p class="pt-5">A 3Tel é uma empresa de tecnologia e serviços de internet. 
-							Em parceria com a ACEDA ela disponibiliza aos associados 50% a mais de VELOCIDADE de internet.
-						</p>
-    				</div>
-    				<div class="col text-center border border-gray border-1 rounded-4 m-1 bg-white pt-1">
-    					<img src="./assets/img/pii.png" class="img-fluid rounded-pill" alt="">
-    					<h3>IndexPii</h3>
-    					<p>A IndexPii é uma empresa de serviços empresariais que realiza controle e gestão de negócio nas áreas administrativa, financeira, contábil e tecnologia. 
+					</p>
+				</div>
+				<div class="col text-center border border-gray border-1 rounded-4 m-1 bg-white pt-5">
+					<img src="./assets/img/3tellogo.png" class="img-fluid rounded-pill" alt="">
+					<h3>3 Tel</h3>
+					<p class="pt-5">A 3Tel é uma empresa de tecnologia e serviços de internet.
+						Em parceria com a ACEDA ela disponibiliza aos associados 50% a mais de VELOCIDADE de internet.
+					</p>
+				</div>
+				<div class="col text-center border border-gray border-1 rounded-4 m-1 bg-white pt-1">
+					<img src="./assets/img/pii.png" class="img-fluid rounded-pill" alt="">
+					<h3>IndexPii</h3>
+					<p>A IndexPii é uma empresa de serviços empresariais que realiza controle e gestão de negócio nas áreas administrativa, financeira, contábil e tecnologia.
 						Em parceria com a ACEDA a IndexPii concede ao associado 10% de desconto nos planos anuais e 5% nos planos mensais em todos os serviços da empresa IndexPii.
-						</p>
-    				</div>
-    			</div>
-		    </div>
+					</p>
+				</div>
+			</div>
+		</div>
 	</section>
 	<!-- /Principais parceiros -->
 
-    <!-- Demais parceiros -->
-    <section>
+	<!-- Demais parceiros -->
+	<section>
 		<div class="container text-center">
 			<div class="row">
 				<div class="col text-center bg-white border border-gray border-1 rounded-4 m-2 p-1 pt-5 ">
 					<img src="./assets/img/acsp.png" class="img-fluid rounded-pill" alt="">
 					<p class="pt-5">Associação Comercial de São Paulo</p>
-					
+
 				</div>
 				<div class="col text-center bg-white border border-gray border-1 rounded-4 m-2 p-1 pt-2">
 					<img src="./assets/img/sebrae.png" class="img-fluid rounded-pill" alt="">
 					<p class="pt-5">SEBRAE AQUI</p>
-					
+
 				</div>
 				<div class="col text-center bg-white border border-gray border-1 rounded-4 m-2 p-1">
 					<img src="./assets/img/levan.png" class="img-fluid rounded-pill" alt="">
 					<p>Levan Seguros</p>
-					
+
 				</div>
-                <div class="col text-center bg-white border border-gray border-1 rounded-4 m-2 p-1">
+				<div class="col text-center bg-white border border-gray border-1 rounded-4 m-2 p-1">
 					<img src="./assets/img/btg.png" class="img-fluid rounded-pill" alt="">
 					<p>BTG Pactual</p>
-					
+
 				</div>
-                <div class="col text-center bg-white border border-gray border-1 rounded-4 m-2 p-1">
+				<div class="col text-center bg-white border border-gray border-1 rounded-4 m-2 p-1">
 					<img src="./assets/img/miura.jpg" class="img-fluid rounded-pill" alt="">
 					<p>Miura</p>
-					
+
 				</div>
 			</div>
 		</div>
 	</section>
-    <!-- /Demais parceiros -->
+	<!-- /Demais parceiros -->
 	<div>
-		<a id="robbu-whatsapp-button" target="_blank" href="https://api.whatsapp.com/send?phone=5511958771996"> 
-            <div class="rwb-tooltip">Fale com a ACEDA</div> 
-            <img src="https://cdn.positus.global/production/resources/robbu/whatsapp-button/whatsapp-icon.svg"> 
-        </a>
+		<a id="robbu-whatsapp-button" target="_blank" href="https://api.whatsapp.com/send?phone=5511958771996">
+			<div class="rwb-tooltip">Fale com a ACEDA</div>
+			<img src="https://cdn.positus.global/production/resources/robbu/whatsapp-button/whatsapp-icon.svg">
+		</a>
 	</div>
 
 
-<!-- Footer -->
-<?php include_once "./template/footer.php" ?>
-<!-- /Footer -->
-<script src="./assets/js/main.min.js?t=1712110939880" crossorigin="anonymous"></script>
+	<!-- Footer -->
+	<?php include_once "./template/footer.php" ?>
+	<!-- /Footer -->
+	<script src="./assets/js/main.min.js?t=1712110939880" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
